81076c4c38d8091329ebd392e379ec7e8233e0c1
Zidane Innis <zidaneinnis@gmail.com>
2023-01-16 19:54:52 -0500

    Working with Multiple Slices

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,15 +6,15 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
     dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch(counterActions.increase(5));
+    dispatch(counterActions.increase());
   };
 
   const decrementHandler = () => {
